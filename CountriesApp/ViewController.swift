b28ebac3bfe4e38f4a575b07d42e5e7eeee45516
Gytis Ptašinskas <51530980+gytisptasinskas@users.noreply.github.com>
2023-11-16 16:36:42 +0200

    Initial Commit

---
+++ CountriesApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CountriesApp
+//
+//  Created by Gytis Ptašinskas on 16/11/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
